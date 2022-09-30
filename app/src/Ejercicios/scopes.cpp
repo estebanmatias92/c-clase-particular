ae310e421f9cae41e791691c360d77a58fd381b2
estebanmatias92 <estebanmatias92@gmail.com>
2022-09-30 17:26:04 +0000

    Modificando ejemplo scopes.cpp

--- app/src/Ejercicios/scopes.cpp
+++ app/src/Ejercicios/scopes.cpp
@@ -5,30 +5,51 @@
 using namespace std;
 
 void saludar();
-void saludarConParametros(string nombre);
+void saludarConParametros(string variable_local);
 
 // Scope GLOBAL
-string nombre = "Carlos";
+string variable_global = "Soy una variable global";
 
 void ejemploScopes()
 {
-    string nombre = "Gregorio"; // LOCAL a esta funcion
+    string variable_local = "Soy una variable local en 'ejemploScopes()'"; // LOCAL a esta funcion
 
+    cout << endl
+         << "INICIO programa principal 'ejemploScopes()':" << endl
+         << endl;
+
+    // Primer llamado a una funcion
     saludar();
 
-    saludarConParametros(nombre); // Pasar variable LOCAL adentro de una funcion
+    cout << endl
+         << endl;
+
+    // Segundo llamado a una funcion
+    saludarConParametros(variable_local); // Pasar variable LOCAL adentro de una funcion
+
+    cout << endl
+         << "FINAL programa principal 'ejemploScopes()'" << endl;
 }
 
 void saludar()
 {
     // Scope LOCAL
-    string nombre2 = "Matias";
-    cout << endl
-         << "Hola " << nombre << endl;
+    string variable_local = "Soy una variable local en 'saludar()'";
+
+    cout << "  INICIO funcion 'saludar()':" << endl
+         << "\tHola!, " << variable_local << endl
+         << "\tHola!, " << variable_global << endl
+         << "  FINAL funcion 'saludar()'" << endl;
 }
 
-void saludarConParametros(string nombre) // Parametros, tambien son LOCALES a la funcion
+void saludarConParametros(string variable_local) // Parametros, tambien son LOCALES a la funcion
 {
-    cout << endl
-         << "Hola " << nombre << endl;
+    // Scope LOCAL
+    string otra_variable_local = "Soy una variable local en 'saludarConParametros(string variable_local)'";
+
+    cout << "  INICIO funcion 'saludarConParametros(string variable_local)':" << endl
+         << "\tHola!, " << variable_local << endl
+         << "\tHola!, " << otra_variable_local << endl
+         << "\tHola!, " << variable_global << endl
+         << "  FINAL funcion 'saludarConParametros(string variable_local)'" << endl;
 }
